060408906a0f8b1b243206abdb0a57e9b861b487
Chris Hannaby <chris.hannaby@netlify.com>
2022-06-09 10:12:51 -0600

    import esm

--- api/functions/utils/postToShopify.js
+++ api/functions/utils/postToShopify.js
@@ -1,26 +1,26 @@
-const fetch = require('node-fetch')
+const fetch = await import("node-fetch");
 
 exports.postToShopify = async ({ query, variables }) => {
   try {
     const result = await fetch(process.env.SHOPIFY_API_ENDPOINT, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token':
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token":
           process.env.SHOPIFY_STOREFRONT_API_TOKEN,
       },
       body: JSON.stringify({ query, variables }),
-    }).then((res) => res.json())
+    }).then((res) => res.json());
 
     if (result.errors) {
-      console.log({ errors: result.errors })
+      console.log({ errors: result.errors });
     } else if (!result || !result.data) {
-      console.log({ result })
-      return 'No results found.'
+      console.log({ result });
+      return "No results found.";
     }
 
-    return result.data
+    return result.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
